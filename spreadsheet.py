8e0cb70c9ff1ac8e83376e19b1fd46c12dac7610
Heikki Vihersalo <heikki@vihersalo.fi>
2024-10-30 19:53:54 +0200

    Implement references

--- spreadsheet.py
+++ spreadsheet.py
@@ -14,36 +14,45 @@ class SpreadSheet:
 
     def evaluate(self, cell: str) -> int | str:
         try:
-            if self.is_formula(cell):
-                cell = cell.replace('=', '')
+            # Handle formula cases
+            if self.isformula(cell):
+                cell = self.handle_simple_formula(cell)
 
-            # ========================================
-            # Handle numbers
-            # ========================================
+            # Handle reference cases
+            if self.isreference(cell):
+                return self.evaluate(self.get(cell))
+
+            # Handle simple int cases
             if cell.isnumeric():
-                return int(cell)
+                return self.handle_simple_int(cell)
 
-            # ========================================
-            # Handle strings
-            # ========================================
-
-            # Feature flags
-            is_correctly_formatted = cell[0] == cell[-1] == "'"
-
-            # Handle correct strings "'Apple'"
-            if is_correctly_formatted:
-                return cell.replace('"', '').replace("'", '')
-
-            # Handle incorrect strings "'Apple"
-            if not is_correctly_formatted:
-                raise ValueError
-
-            # ========================================
-            # Handle formulas
-            # ========================================
+            # Handle simple string cases
+            return self.handle_simple_strings(cell)
 
         except ValueError:
             return '#Error'
 
-    def is_formula(self, cell: str) -> bool:
+    def isformula(self, cell: str) -> bool:
         return cell[0] == '='
+
+    def isreference(self, cell: str) -> bool:
+        letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        return cell[0] in letters and cell[1:].isnumeric()
+
+    def handle_simple_formula(self, cell: str) -> int | str:
+        return cell.replace('=', '')
+
+    def handle_simple_strings(self, cell: str) -> str:
+        # Feature flags
+        is_correctly_formatted = cell[0] == cell[-1] == "'"
+
+        # Handle correct strings "'Apple'"
+        if is_correctly_formatted:
+            return cell.replace('"', '').replace("'", '')
+
+        # Handle incorrect strings "'Apple"
+        if not is_correctly_formatted:
+            raise ValueError
+
+    def handle_simple_int(self, cell: str) -> int:
+        return int(cell)
--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -31,3 +31,9 @@ class TestSpreadSheet(TestCase):
     def test_cell_content_is_simple_formula_error(self):
         spreadsheet = SpreadSheet()
         self.assertEqual('#Error', spreadsheet.evaluate("='Apple"))
+
+    def test_cell_references_simple_formula(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1", "=B1")
+        spreadsheet.set("B1", "42")
+        self.assertEqual(42, spreadsheet.evaluate("A1"))
